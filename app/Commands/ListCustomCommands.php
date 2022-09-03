c20e49d9b82e0586479c0d3a53d1cf8d1b2e1f59
Raúl López <hi@raullg.com>
2022-09-03 21:20:18 +0200

    list custom commands

---
+++ app/Commands/ListCustomCommands.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Commands;
+
+use App\Genesis\Actions\GetCustomCommands;
+use LaravelZero\Framework\Commands\Command;
+
+class ListCustomCommands extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'commands';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Lists all available custom commands built in Genesis.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $commands = GetCustomCommands::run();
+        $headers = ['Signature', 'Description'];
+        $rows = collect($commands)->map(fn ($command) => [
+            $command->signature . "\n",
+            $command->description,
+        ])->toArray();
+        $this->table($headers, $rows);
+        $this->info('You can run a custom command by running `genesis run <signature>`');
+    }
+}
---
+++ app/Genesis/Actions/GetCustomCommands.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Genesis\Actions;
+
+use App\Genesis\CustomCommand;
+use App\Genesis\Genesis;
+
+class GetCustomCommands {
+    use AsAction;
+
+    public function __invoke()
+    {
+        $genesis = new Genesis();
+        $response = $genesis->client()->get('/custom-commands')->json();
+        return array_map(fn ($command) => CustomCommand::from($command), $response['commands']);
+    }
+}
